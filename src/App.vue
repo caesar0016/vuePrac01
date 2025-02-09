34c11e36ef76fa8d84706985d3771cd6dbff926f
caesar.dapiton <dapitoncaesar26@gmail.com>
2025-02-09 16:28:10 +0800

    Update: Simple crud vue.

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,61 @@
-<script>
-  export default{
-    data(){
-      return {
-        name: 'Hu Tao',
-        youLikeher: 'No',
-        gender: 'Female',
-        height: 'medium',
-        supports: ['Xing Qiu', 'Furina', 'Xilonen'],
-        links: 'https://game8.co/games/Genshin-Impact/archives/314347'
-      };
-    },
-    methods:{
-      toggleLike(){
+<script setup>
+import { ref } from 'vue';
 
-        if(this.youLikeher === 'Yes'){
+const name = ref('Jean');
+const status = ref('Active');
+const nation = ref('Mondstadt');
+const vision = ref('Anemo');
+const race = ref('Human');
+const others = ref(['Tall Girl', 'Leo Minor', 'Invigorating Pizza']);
 
-          this.youLikeher = 'No';
+// Create a ref to store the new detail input
+const newDetail = ref('');
 
-        }else if(this.youLikeher === 'No'){
+// Toggle the status between 'Active' and 'Inactive'
+const toggleGetJean = () => {
+  if (status.value === 'Active') {
+    status.value = 'Inactive';
+  } else if (status.value === 'Inactive') {
+    status.value = 'Active';
+  }
+};
 
-          this.youLikeher = 'Yes';
+// Add new detail to the list
+const addDetails = () => {
+  if (newDetail.value.trim() !== '') {
+    others.value.push(newDetail.value);
+    newDetail.value = '';  // Clear the input after adding
+  }
+};
 
-        }else{
-
-          this.youLikeher = 'Yes';
-
-        } 
-      }
-    }
-  };
+const delDetails = (index) =>{
+  others.value.splice(index, 1);
+};
 </script>
 
-<template>  
+<template>
   <h1>Genshin Daily</h1>
   <h1> Name: {{ name }}</h1>
-  <p v-if="youLikeher === 'Yes'">Do I like Hu Tao? {{ youLikeher }}</p>
-  <p v-else-if="youLikeher === 'No'">Do I like Hu Tao? {{ youLikeher }}</p>
-  <p v-else="youLikeher">No Comment</p>
+  <h1> Status: {{ status }}</h1>
 
   <h3>Supports:</h3>
   <ul>
-    <li v-for="character in supports" :key="supp01">{{ character }}</li>
+    <li v-for="(traits, index) in others" :key="index">
+      <span>
+        {{ traits }}
+      </span>
+      <button @click="delDetails(index)">x</button>
+    </li>
   </ul>
-  <!-- <a v-bind:href="links">See more for Hu Tao</a> -->
-  <a :href="links">See more for Hu Tao</a>
-
+  
   <div>
-
-    <button @click="toggleLike">Click to like hu tao</button>
+    <button @click="toggleGetJean">Click to include Jean</button>
   </div>
+
+  <form @submit.prevent="addDetails">
+    <label for="newDetails">Add more</label><br>
+    <!-- Bind the input to the 'newDetail' ref using v-model -->
+    <input type="text" id="newDetails" v-model="newDetail"><br>
+    <button type="submit">Add</button>
+  </form>
 </template>
---
+++ src/App1.vue
@@ -0,0 +1,52 @@
+<script>
+  export default{
+    data(){
+      return {
+        name: 'Hu Tao',
+        youLikeher: 'No',
+        gender: 'Female',
+        height: 'medium',
+        supports: ['Xing Qiu', 'Furina', 'Xilonen'],
+        links: 'https://game8.co/games/Genshin-Impact/archives/314347'
+      };
+    },
+    methods:{
+      toggleLike(){
+
+        if(this.youLikeher === 'Yes'){
+
+          this.youLikeher = 'No';
+
+        }else if(this.youLikeher === 'No'){
+
+          this.youLikeher = 'Yes';
+
+        }else{
+
+          this.youLikeher = 'Yes';
+
+        } 
+      }
+    }
+  };
+</script>
+
+<template>  
+  <h1>Genshin Daily</h1>
+  <h1> Name: {{ name }}</h1>
+  <p v-if="youLikeher === 'Yes'">Do I like Hu Tao? {{ youLikeher }}</p>
+  <p v-else-if="youLikeher === 'No'">Do I like Hu Tao? {{ youLikeher }}</p>
+  <p v-else="youLikeher">No Comment</p>
+
+  <h3>Supports:</h3>
+  <ul>
+    <li v-for="character in supports" :key="supp01">{{ character }}</li>
+  </ul>
+  <!-- <a v-bind:href="links">See more for Hu Tao</a> -->
+  <a :href="links">See more for Hu Tao</a>
+
+  <div>
+
+    <button @click="toggleLike">Click to like hu tao</button>
+  </div>
+</template>
